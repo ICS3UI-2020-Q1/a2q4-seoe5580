08ad2bf3f5c088506bed89cc4d73be8bf2d334be
seoe5580 <seoe5580@wrdsb.ca>
2020-09-15 19:57:24 +0000

    Finished money discount assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Allows you to calculate the deducted price of an item
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +11,36 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user to enter the amount of money the customer spent
+    System.out.println("Please enter the amount the customer spent");
     
+    // Gets the amount of money the customer spent
+    double money = input.nextDouble();
+
+    // Calculates the money that they save and shows the total
+    if (money > 120){
+      System.out.println("They will receive 40% off");
+      System.out.println("They will save " + money * 0.4);
+      System.out.println("The new total is " + (money - money * 0.4));
+    }
+    if (money > 80){
+      System.out.println("They will receive 30% off");
+      System.out.println("They will save " + money * 0.3);
+      System.out.println("The new total is " + (money - money * 0.3));
+    }
+    if (money > 40){
+      System.out.println("They will receive 20% off");
+      System.out.println("They will save " + money * 0.2);
+      System.out.println("The new total is " + (money - money * 0.2));
+    }
+    if (money >= 0.01){
+      System.out.println("They will receive 10% off");
+      System.out.println("They will save " + money * 0.1);
+      System.out.println("The new total is " + (money - money * 0.1));
+    }
+
   }
 }
